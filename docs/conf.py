2525ede882d9a866f12dd578cb0b8316a6466e10
Jed Carlson <jedidiah@umich.edu>
2017-07-03 01:54:16 -0400

    Mon Jul  3 01:54:16 2017: test rtd

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,15 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+# source_suffix = '.rst'
+
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
+source_suffix = ['.rst', '.md']
 
 # The master toctree document.
 master_doc = 'index'
@@ -168,6 +176,3 @@ texinfo_documents = [
      author, 'smaug', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
